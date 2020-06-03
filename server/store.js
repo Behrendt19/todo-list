85e5cb4c3db3b795a35ae9e6f7299c2fdd5261e7
Behrendt19 <johndoe@example.com>
2020-06-03 16:09:48 +0200

    Dodano logowanie operacji na tabeli

--- server/store.js
+++ server/store.js
@@ -1,6 +1,16 @@
+function LoggingFilter() {
+  this.handle = (requestOptions, next) => {
+    console.log(requestOptions);
+    next(requestOptions, (returnObject, finalCallback, next) => {
+      console.log(returnObject);
+    })
+  }
+}
+
 const storage = require('azure-storage')
 const again = new storage.LinearRetryPolicyFilter();
-const service = storage.createTableService().withFilter(again);
+const loggingOp = new LoggingFilter();
+const service = storage.createTableService().withFilter(again).withFilter(loggingOp);
 const table = 'tasks'
 
 const init = async () => (
