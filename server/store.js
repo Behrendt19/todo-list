73e9441fc1f6dc89c0ac2a0a52f4b11df2d41ee7
Behrendt19 <johndoe@example.com>
2020-06-03 17:35:06 +0200

    Dodano przycisk zmiany statusu zadania

--- server/store.js
+++ server/store.js
@@ -47,11 +47,12 @@ const addTask = async ({ title, description}) => (
 const listTasks = async () => (
   new Promise((resolve, reject) => {
     const query = new storage.TableQuery()
-      .select(['title', 'description', 'Timestamp'])
+    .select(['title', 'description', 'Timestamp', 'RowKey'])
       .where('PartitionKey eq ?', 'task')
 
     service.queryEntities(table, query, null, (error, result) => {
       !error ? resolve(result.entries.map((entry) => ({
+        id: entry.RowKey._,
         title: entry.title._,
         description: entry.description._,
         Timestamp: entry.Timestamp._
@@ -59,8 +60,22 @@ const listTasks = async () => (
     })
   })
 )
+const deleteTask = async ({ id }) => (
+  new Promise((resolve, reject) => {
+    const gen = storage.TableUtilities.entityGenerator
+    const task = {
+      PartitionKey: gen.String('task'),
+      RowKey: gen.String(id)
+    }
+
+    service.deleteEntity(table, task, (error) => {
+      !error ? resolve() : reject()
+    })
+  })
+)
 module.exports = {
   init,
   addTask,
   listTasks,
+  deleteTask
 } 
